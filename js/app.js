b0a8c122224f86b9f96346da7f205f3a2d0c82db
Michael Churchill <michaelchurchill@Michael.local>
2016-04-05 16:37:28 -0500

    cleaning up any staged and uncomitted changes

--- js/app.js
+++ js/app.js
@@ -56,6 +56,7 @@ function app() {
     	_handleSignUp: function(evt){
 		    var component = this
 		    var evt = evt
+		    console.log("event!", evt)
 		    evt.preventDefault()
 
 		    var emailInput    = evt.currentTarget.email.value
@@ -68,19 +69,23 @@ function app() {
 		        email    : emailInput,  // internal to fb
 		        password : pwInput //internal to fb
 		      }, 
-		    function(err, authData){
-		       
-		    	var userSecretsColl = new UserSecretsColl(fbRef);
 
-		        userSecretsColl.create({
+		    function(err, authData){
+		       console.log(err)
+		       console.log(authData)
+		       var userBlogsColl = new UserBlogsColl(fbRef);
+
+		        userBlogsColl.create({
 		           username: userNameInput,
-		           
-		           uid: authData.uid
+		           id: authData.uid,
+		           posts: ''
 		        })
 
 		         //notify app of authenticatedUser
-		        appRtr.authenticatedUser = authData.uid
-		        appRtr.navigate('',{trigger: true})
+		        myRouter.authenticatedUser = authData.uid
+		        myRouter.authenticatedUserPosts = new PostsByUserColl(fbRef, authData.uid)
+
+		        myRouter.navigate('', {trigger: true} )
 
 		    })
 		},
@@ -88,7 +93,7 @@ function app() {
 		componentDidMount: function(){
 		    var component = this
 		    this.props.coll.on('sync', function(){
-		        console.log("SEEECRETS:", component.props.coll)
+		        console.log("mounted", component.props.coll)
 		      	component.setState({
 		        	userSecretColl: component.props.coll
 		    	})
@@ -99,19 +104,18 @@ function app() {
 		    return (
 		      <div>
 		        <form onSubmit={this._handleSignUp}>
-		          <h3 className="signup">Welcome to Another Blog Site!</h3>
+		          <h3>Welcome to Another Blog Site!</h3>
 		          <input type="text" id="email" placeholder="Email"/><br/>
 		          <input type="password" id="password" placeholder="Password"/><br/><br/>
 
 		          <input type="text" id="username" placeholder="Your Name"/><br/>
-		          <input className="button-primary" type="submit" defaultValue="Sign Up"/><br/>
+		          <input id="signup" type="submit" defaultValue="Sign Up"/><br/>
 
 		        </form>
 		        <hr/>
 		        <form onSubmit={this._handleLogIn}>
 		          <h3 className="signin">Sign In</h3>
 		          <input type="text" id="email" placeholder="Email"/><br/>
-		          <input type="password" id="password" placeholder="Password"/><br/>
 		          <input className="button-primary" type="submit" defaultValue="Log In"/><br/>
 		        </form>
 		      </div>
@@ -120,19 +124,57 @@ function app() {
     })
 
     var HomeView = React.createClass({
+    	getInitialState: function(){	
+    		return {
+    			posts: this.props.postsColl
+    		}
+    	},
+
+    	// componentDidMount: function(){
+    	// 	var self = this
+    	// 	this.props.postsColl.on('sync', function(){
+
+    	// 	})
+    	// },
+
+    	_addPost: function(e) {
+    		console.log("new post")
+
+    		this.props.postsColl.add({
+    			content: e.currentTarget.value
+    		})
+    	},
+
     	render: function() {
-    		console.log(home)
+
+    		console.log('home')
+    		var coll = this.props //.get('id')
+    		console.log(coll)
     		return (
-    			<div id="homeDiv"></div>
+    			<div id="homeDiv">
+    				<div>Your posts go here</div>
+    				<input onKeyPress={this._addPost}type="text"></input>
+    			</div>
     		)
     	}
     })
-    var UserSecretsColl = BackboneFire.Firebase.Collection.extend({
+
+    var UserBlogsColl = BackboneFire.Firebase.Collection.extend({
   		url: "",
   		initialize: function(fb){
-    		this.url = fb.child('blogPost')
+    		this.url = fb.child('users')
   		}
 	})
+
+	var PostsByUserColl = BackboneFire.Firebase.Collection.extend({
+		url: "",
+		initialize: function(fb, usrUid){
+  		this.url = fb.child('users').child(usrUid).child('posts')
+  		console.log(this.url)
+  	}
+	})
+
+
 
     var AppRouter = BackboneFire.Router.extend({
     	routes: {
@@ -145,13 +187,17 @@ function app() {
     			this.navigate('authenticate', {'trigger': true})
     			return
     		}
-    		var fbColl = new UserSecretsColl(fbRef)
-    		DOM.render(<HomeView coll={fbColl}/>, document.querySelector('.container'))
+ 			
+
+    		DOM.render(<HomeView postsColl={this.authenticatedUserPosts}/>, document.querySelector('.container'))
     	},
 
     	showSplash: function() {
     		console.log("work please")
-    		 DOM.render(<AuthView />, document.querySelector('.container'))
+    		
+    		var fbColl = new UserBlogsColl(fbRef)
+    		console.log(fbColl)
+    		 DOM.render(<AuthView coll={fbColl}/>, document.querySelector('.container'))
     	},
 
     	initialize: function() {
@@ -160,6 +206,7 @@ function app() {
       			if(authData){
 		        	console.log('authenticated')
 		        	rtr.authenticatedUser = authData.uid
+
 		      	} else {
 		        	console.log('Not authenticated')
 		        	rtr.authenticatedUser = null
